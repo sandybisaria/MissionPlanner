cf4cefa636048b6ac153aedf8dc56117d5379e17
Hima T <himat15@yahoo.com>
2014-01-07 13:37:52 -0500

    Edited help tab text

--- GCSViews/Help.cs
+++ GCSViews/Help.cs
@@ -63,5 +63,10 @@ namespace MissionPlanner.GCSViews
             MissionPlanner.Utilities.Update.dobeta = true;
             MissionPlanner.Utilities.Update.DoUpdate();
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
