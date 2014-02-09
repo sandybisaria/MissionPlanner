48b944ea70de73f086da791447d0e8356e72af3f
Hima T <himat15@yahoo.com>
2014-02-09 16:24:37 -0500

    Started scriptVideoInterface

---
+++ ScriptVideoInterface.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronPython.Hosting;
+using System.IO;
+using System.Windows.Forms; 
+using System.Threading;
+using MissionPlanner;
+
+namespace MissionPlanner
+{
+    public class ScriptVideoInterface
+    {
+
+        Script script = new Script();
+
+        DateTime timeout = DateTime.Now;
+        static Microsoft.Scripting.Hosting.ScriptEngine engine;
+        static Microsoft.Scripting.Hosting.ScriptScope scope;
+
+        public void startVideoRead()
+        {
+            while (true)
+            {
+                script.runScript(script);
+            }
+        }
+
+        public void turn(String dir)
+        {
+            switch(dir)
+            {
+                case "right":
+                    script.SendRC(4, 2000, true);
+                    break;
+                case "left":
+                    script.SendRC(4, 2000, true);
+                    break;
+                case "stop":
+                    script.SendRC(3, 0, true);
+                    break;
+            }
+        }
+
+
+        private void BUT_run_script_Click(object sender, EventArgs e)
+        {
+            Thread scriptThread;
+            string command;
+
+            try
+            {
+                command = File.ReadAllText("videoDirections.txt");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open script file due to exception: " + ex.Message);
+                return;
+            }
+
+            scriptThread = new System.Threading.Thread(new System.Threading.ThreadStart(run_selected_script))
+            {
+                IsBackground = true,
+                Name = "Script Thread (new)"
+            };
+            labelScriptStatus.Text = "Script Status: Running";
+
+            script = null;
+            outputwindowstarted = false;
+
+            scriptThread.Start();
+            scriptrunning = true;
+            scriptChecker.Enabled = true;
+            checkBoxRedirectOutput.Enabled = false;
+
+
+            //remember this
+            scriptThread.Abort();
+        }
+
+    }
+}
